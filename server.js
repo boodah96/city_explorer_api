c930acb5febf48354c15ed6621722a1bff510521
boodah96 <2014990065@ses.yu.edu.jo>
2021-02-21 16:24:23 +0200

    add location

--- server.js
+++ server.js
@@ -15,6 +15,21 @@ const server= express();
      res.send('pass rout')
  })
  
+server.get('/location',(req,res)=>{
+    const locData= require('./data/location.json')
+    const location=new Location(locData);
+    res.send(location)
+})
+
+function Location(locPage){
+    this.search_query='Lynnwood';
+    this.formatted_query=locPage[0].display_name;
+   this.latitude=locPage[0].lat;
+  this.longitude=locPage[0].lon;
+}
+
+
+
 server.listen(PORT,()=>{
     console.log(`listening on PORT ${PORT}`)
 })
